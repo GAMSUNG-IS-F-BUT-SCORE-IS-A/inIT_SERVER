9acbce7bc2bdfaa16b5d8f7c03b0aacd1be91470
lhb8106 <lhb8106@naver.com>
2022-06-17 21:13:28 +0900

    ip

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ async function readImageFile(file){
 //서버 실행
 //각자 ip주소 넣기, port: 3006 변경 금지!
 //학교: 172.18.9.151  집: 172.30.1.25
-app.listen(3006, '192.168.100.17', (err)=> {
+app.listen(3006, '192.168.100.3', (err)=> {
     if(!err) {
         console.log('server start');
     }
