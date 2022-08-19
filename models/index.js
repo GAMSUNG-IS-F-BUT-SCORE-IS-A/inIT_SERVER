b9a1d2fab429509f055f723d2cd7f3664e7f1a62
yoon <sj87at@gmail.com>
2022-08-19 14:55:02 +0900

    [FEAT] #18 - setting for aws upload

--- models/index.js
+++ models/index.js
@@ -3,6 +3,9 @@
 'use strict';
 
 const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+
 const Member = require('./members');
 const ProjectInfo = require('./projectinfo');
 const Zzim = require('./zzim');
@@ -11,8 +14,6 @@ const Recruit = require('./recruit');
 const Todo = require('./todo');
 const Evaluation = require('./evaluation');
 
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
